c73cda58d4a1052859375f2a9b73eca8876c1edd
Powerjagger <gre23005@byui.edu>
2024-05-03 20:03:57 -0600

    Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the Program!");
+        Console.WriteLine("What can I call you?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hello, {name}, I am designated Prep Program 5.");
+        
+        Console.WriteLine("Weird question, but what is your favorite number?");
+        int favoriteNumber = int.Parse(Console.ReadLine()); 
+
+        int favoriteSquared = favoriteNumber * favoriteNumber; 
+        
+        Console.WriteLine($"Oh, interesting! So {favoriteNumber} is your favorite.");
+        Console.WriteLine($"Did you know that your favorite number squared is {favoriteSquared}");
     }
 }
